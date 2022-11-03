56e35dcac1976de4f7755a632775c06f171df40a
Denis Borodachenko <borodachenko-dv@sber-zvuk.com>
2022-11-02 20:08:49 -0700

    Initial Commit

---
+++ Lesson3.Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson3.Alert
+//
+//  Created by Даниил Димов on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
