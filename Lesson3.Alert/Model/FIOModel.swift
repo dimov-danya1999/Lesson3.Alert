581fe3788b303222c25569ab88cf89e7e7eb38c5
Denis Borodachenko <borodachenko-dv@sber-zvuk.com>
2022-11-04 15:16:33 -0700

    no message

---
+++ Lesson3.Alert/Model/FIOModel.swift
@@ -0,0 +1,15 @@
+//
+//  FIO Model.swift
+//  Lesson3.Alert
+//
+//  Created by Даниил Димов on 04.11.2022.
+//
+
+import Foundation
+
+struct FIOModel {
+    var family: String?
+    var name: String?
+    var father: String?
+    
+}
